fc006d4b3f679ce29487f9e8b8ffcf1920c162c7
Analyn Sosa <analyn_sosa@ussc.com.ph>
2017-05-17 09:41:46 +0800

    views/main/homepage.php

--- application/views/main/homepage.php
+++ application/views/main/homepage.php
@@ -1,44 +1,31 @@
 <div class="col-xs-12">
-    <div class="box box-primary">
+    <center><h1 class="box-title"> USSC RAFFLE </h1></center>
 
-        <div class="box-header with-border box-info">
-            <h3 class="box-title" text="center">USSC RAFFLE</h3>
-                <div class="box-tools">
-                </div>
-        </div>
-
-        <div class"box-body">
-        </div>
-
-        <div class="box-body table-responsive">
-          
-            <table class="table text-center" align="center" >
-                <tbody>
-                    <tr>
-                        <th>Raffle Entries</th>
-                        <th>Raffle Draw</th>
-                        <th>Raffle Reports</th>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="<?=base_url()?>home/entries" class="btn btn-primary btn-xs" id="btnHome" name="btnHome" type="button">
-                                <img src="<?=base_url()?>assets/images/ticket.png" height="160" width="160"></button>
-                            </a>
-                        </td>
-                        <td>
-                             <a href="<?=base_url()?>home/draw" class="btn btn-primary btn-xs" id="btnHome" name="btnHome" type="button" style="margin:20px auto text-align:center display:block width:120px">
-                                <img src="<?=base_url()?>/assets/images/raffle.png" height="160" width="160"></button>
-                            </a>
-                        </td>
-                        <td>
-                             <a href="<?=base_url()?>home/report" class="btn btn-primary btn-xs" id="btnHome" name="btnHome" type="button" style="margin:20px auto text-align:center display:block width:120px">
-                                <img src="<?=base_url()?>/assets/images/report.png" height="160" width="160"></button>
-                            </a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table> 
-
-        </div>
+    <div class="box-body">
+        <table class="table text-center" align="center">
+            <tr>
+                <th>RAFFLE ENTRIES</th>
+                <th>RAFFLE DRAW</th>
+                <th>REPORTS</th>
+            </tr>
+            <tr>
+                <td>
+                    <a href="<?=base_url()?>home/entries" class="btn btn-primary btn-xs">
+                        <img src="<?=base_url()?>assets/images/ticket.png" height="200" width="200">
+                    </a>
+                </td>
+                <td>
+                     <a href="<?=base_url()?>home/draw" class="btn btn-primary btn-xs" style="margin:20px auto text-align:center display:block width:120px">
+                        <img src="<?=base_url()?>/assets/images/raffle.png" height="200" width="200">
+                    </a>
+                </td>
+                <td>
+                     <a href="<?=base_url()?>home/report" class="btn btn-primary btn-xs" style="margin:20px auto text-align:center display:block width:120px">
+                        <img src="<?=base_url()?>/assets/images/report.png" height="200" width="200">
+                    </a>
+                </td>
+            </tr>
+        </table> 
     </div>
+
 </div>  
