b9992c3448e9c95468b54febc1d4fd8bad5324bc
Analyn Sosa <analyn_sosa@ussc.com.ph>
2017-05-17 11:25:33 +0800

    error and success

--- application/views/main/entry.php
+++ application/views/main/entry.php
@@ -14,15 +14,19 @@
         <form class="form-horizontal" method="POST" action="#" id="entry_form" name="entry_form" enctype="multipart/form-data">
             <div class="box-body">
                 <label class="col-sm-2 control-label">Promo Title</label>
-                <select class="col-sm-4" id="category">
-                    <option value="0">--- SELECT ---</option>
-                    <option value="pw_upgrade">USSC PanaloWallet Upgrade</option>
-                </select>
+                <div class="col-sm-3">
+                    <select class="form-control" id="category">
+                        <option value="0">--- SELECT ---</option>
+                        <option value="upgrade">USSC PanaloWallet Upgrade</option>
+                    </select>
+                </div>
             </div>
             <div class="box-body">
                 <label class="col-sm-2 control-label" for="filename">File</label>
-                <input type="file" class="file" id="filename" name="filename" required>
-                <input type="text" class="hide" id="promo_desc" name="promo_desc" required>
+                <div class="col-sm-3">
+                    <input type="file" class="file" id="filename" name="filename" required>
+                    <input type="text" class="hide" id="promo_desc" name="promo_desc" required>
+                </div>
             </div>
         </form>
 
